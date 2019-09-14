6c5e76e1f60756c2b27e0ea6c9693c07c14c9ccb
netoap <alexanderneto5@gmail.com>
2019-09-14 20:02:40 +0100

    Primeiro commit - projeto criado

---
+++ src/main/java/com/alex/tutomongo/TutomongoApplication.java
@@ -0,0 +1,13 @@
+package com.alex.tutomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutomongoApplication.class, args);
+	}
+
+}
