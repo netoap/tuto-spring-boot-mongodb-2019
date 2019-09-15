6114ffd7795f909019a64e54765ffb710c5b1555
netoap <alexanderneto5@gmail.com>
2019-09-15 22:32:57 +0100

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/alex/tutomongo/resourses/UserResource.java
+++ src/main/java/com/alex/tutomongo/resourses/UserResource.java
@@ -1,6 +1,8 @@
 package com.alex.tutomongo.resourses;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.tutomongo.domain.User;
+import com.alex.tutomongo.dto.UserDTO;
 import com.alex.tutomongo.services.UserService;
 
 @RestController
@@ -19,8 +22,9 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	ResponseEntity<List<User>> findAll(){
+	ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
