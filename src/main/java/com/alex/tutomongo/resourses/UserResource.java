f1dffb7f8c6f67ac5306a329b780f9f7aad8cb9d
netoap <alexanderneto5@gmail.com>
2019-09-16 01:26:47 +0100

    Obtendo um utilizador por id

--- src/main/java/com/alex/tutomongo/resourses/UserResource.java
+++ src/main/java/com/alex/tutomongo/resourses/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +23,15 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	ResponseEntity<List<UserDTO>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/alex/tutomongo/services/UserService.java
+++ src/main/java/com/alex/tutomongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.alex.tutomongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alex.tutomongo.domain.User;
 import com.alex.tutomongo.repository.UserRepository;
+import com.alex.tutomongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(()-> new ObjectNotFoundException("Objecto Nao Encontrado"));
 	}
 
 }
